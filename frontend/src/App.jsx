eb92677b4938c405c635f1c6b1f0b2c0911782f9
Autumnsky-K <autumnsky1562@gmail.com>
2026-06-15 14:48:46 +0900

    feat: Live2D 챗봇 껍데기 구현

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,37 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom'
 
 import ArtistPage from './features/artist/artist.tsx'
 import GoodsDetailPage from './features/goods/GoodsDetailPage.jsx'
 import GoodsPage from './features/goods/goods.jsx'
 import LoginPage from './features/member/LoginPage.jsx'
 import SignupPage from './features/member/SignupPage.jsx'
+import VtuberChatbot from './shared/components/VtuberChatbot.tsx'
+
+function AppShell() {
+  const { pathname } = useLocation()
+  const hideVtuber = pathname === '/login' || pathname === '/signup'
+
+  return (
+    <>
+      <div id='content'>
+        <Routes>
+          <Route path="/" element={<GoodsPage />} />
+          <Route path="/goods" element={<GoodsPage />} />
+          <Route path="/goods/:goodsId" element={<GoodsDetailPage />} />
+          <Route path="/artists" element={<ArtistPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+        </Routes>
+      </div>
+      {!hideVtuber && <VtuberChatbot />}
+    </>
+  )
+}
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<GoodsPage />} />
-        <Route path="/goods" element={<GoodsPage />} />
-        <Route path="/goods/:goodsId" element={<GoodsDetailPage />} />
-        <Route path="/artists" element={<ArtistPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-      </Routes>
+      <AppShell />
     </BrowserRouter>
   )
 }
