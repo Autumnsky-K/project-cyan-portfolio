f8e768ab3b1b26dc246415bc77836d004f31d043
ykgoh3 <ykgoh3@gmail.com>
2026-06-19 12:46:55 +0900

    chore: 스토어 기본 경로와 페이지 제목 정리

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom'
+import { useEffect } from 'react'
+import { BrowserRouter, Navigate, Route, Routes, useLocation } from 'react-router-dom'
 
 import ArtistPage from './features/artist/artist.tsx'
 import CartPage from './features/cart/CartPage'
@@ -13,11 +14,28 @@ function AppShell() {
   const { pathname } = useLocation()
   const hideVtuber = pathname === '/login' || pathname === '/signup'
 
+  useEffect(() => {
+    const pageName = pathname.startsWith('/goods/')
+      ? '상품 상세'
+      : pathname === '/goods'
+        ? '상품'
+        : pathname === '/artists'
+          ? '아티스트'
+          : pathname === '/cart'
+            ? '장바구니'
+            : pathname === '/login'
+              ? '로그인'
+              : pathname === '/signup'
+                ? '회원가입'
+                : '스토어'
+    document.title = `Project Cyan Store - ${pageName}`
+  }, [pathname])
+
   return (
     <>
       <div id='content'>
         <Routes>
-          <Route path="/" element={<GoodsPage />} />
+          <Route path="/" element={<Navigate replace to="/goods" />} />
           <Route path="/goods" element={<GoodsPage />} />
           <Route path="/goods/:goodsId" element={<GoodsDetailPage />} />
           <Route path="/cart" element={<CartPage />} />
